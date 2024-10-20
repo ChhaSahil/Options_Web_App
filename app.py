7e11d268869b14c8701190a2e508a9fae4aa73ba
Sahil Chhabra <96775596+ChhaSahil@users.noreply.github.com>
2024-10-20 12:56:36 +0000

    done2

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ S=0.0
 if options_formula == "Black & Scholes":
     symbol = st.text_input("Symbol of the Underlying(Equity) - ",placeholder="Enter the NSE symbol of the equity")
     if symbol:
-        if symbol in ind_symbol.keys():
+        if symbol in ind_symbol:
             symbol_NS = ind_symbol[symbol]
         elif ".NS" not in symbol:
            symbol_NS = symbol+".NS"
